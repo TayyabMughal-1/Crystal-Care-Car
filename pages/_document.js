837a46de75e5629311f716a47e3b37669705a342
TayyabMughal-1 <tayyabmughal486@gmail.com>
2026-06-29 12:31:03 +0500

    font changed

--- pages/_document.js
+++ pages/_document.js
@@ -13,7 +13,7 @@ export default function Document() {
           crossOrigin="anonymous"
         />
         <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&family=Playfair+Display:wght@500;600;700;800;900&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Barlow:wght@300;400;500;600;700;800&family=Barlow+Condensed:wght@600;700;800;900&display=swap"
           rel="stylesheet"
         />
       </Head>
